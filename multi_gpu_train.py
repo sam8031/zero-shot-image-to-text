2dbb4e4f13a50269dc8b5de48cd2e9486c53813d
sam8031 <sam8031@g.rit.edu>
2024-04-07 14:36:41 -0400

    update

--- multi_gpu_train.py
+++ multi_gpu_train.py
@@ -92,7 +92,7 @@ def train():
 
     loss_img = nn.CrossEntropyLoss()
     loss_caption = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=5e-5, betas=(0.9,0.98), eps=1e-6, weight_decay=0.05)
+    optimizer = Adam(model.parameters(), lr=5e-6, betas=(0.9,0.98), eps=1e-6, weight_decay=0.05)
 
     # Training loop
     model.train()
