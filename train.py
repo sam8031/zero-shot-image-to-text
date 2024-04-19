72a59d5bd9466abbca2ebc8b8259a9ccaffd12b7
sam8031 <sam8031@g.rit.edu>
2024-04-18 22:58:55 -0400

    train update

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def train():
 
     loss_img = nn.CrossEntropyLoss()
     loss_caption = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=5e-4,betas=(0.9,0.98),eps=1e-6,weight_decay=0.2)
+    optimizer = Adam(model.parameters(), lr=5e-5,betas=(0.9,0.98),eps=1e-6,weight_decay=0.2) #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
 
     # Training loop
     start_time = time.time()
